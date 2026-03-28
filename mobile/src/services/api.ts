dd007a88406dbf61a060399d2b4dcde74b402e57
Miguel <miguelrafaelor@gmail.com>
2026-03-28 16:19:54 -0300

    att url

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -4,7 +4,7 @@ const getBaseURL = () => {
     if (__DEV__) {
         return "http://192.168.15.177:3333";
     }
-    return "https://nearby-5o2p1zn6o-mror21s-projects.vercel.app/";
+    return "https://nearby-5o2p1zn6o-mror21s-projects.vercel.app";
 };
 
 export const api = axios.create({
