8e6be660ef347b82c0d95ec42e37562afbff4100
Miguel <miguelrafaelor@gmail.com>
2026-03-29 00:36:11 -0300

    luz na fim do tunel 333333

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -4,7 +4,7 @@ const getBaseURL = () => {
     if (__DEV__) {
         return "http://192.168.15.177:3333";
     }
-    return "https://nearby-eight.vercel.app";
+    return "https://nearby-production-329b.up.railway.app";
 };
 
 export const api = axios.create({
