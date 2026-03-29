7b7e5b07cfb7207bdc8f863fe46111560c9a60fe
Miguel <miguelrafaelor@gmail.com>
2026-03-29 00:56:47 -0300

    luz na fim do tunel 33333333

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-const getBaseURL = () => {
-    if (__DEV__) {
-        return "http://192.168.15.177:3333";
-    }
-    return "https://nearby-production-329b.up.railway.app";
-};
-
+// Forçamos o link do Railway para o celular buscar na nuvem
 export const api = axios.create({
-    baseURL: getBaseURL(),
+    baseURL: "https://nearby-production-329b.up.railway.app",
     timeout: 10000,
 });
